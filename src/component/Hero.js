95b9ed5f21a2c3e4a08bf765b9a15679260e5a9d
John Alvin <johnalvin.bonifacio@proton.me>
2022-11-17 10:51:11 +0800

    structure for hero component
    set container as main parent element
    add images files for main image
    add aside element for side panel
    add images and details for location of the main image
    structure of the component

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -2,8 +2,25 @@ import React from "react";
 
 function Hero() {
   return (
-    <div>
-      <h1>this is Hero </h1>
+    <div className="container">
+      <img src="../images/places" alt="beautiful-place" />
+
+      <aside>
+        <div className="top-head">
+          <img src="../images/location-logo.png" alt="location-logo" />
+          <p>Japan</p>
+          <p>view on google maps</p>
+        </div>
+        <div className="main-places">
+          <h1>Mount Fuji</h1>
+          <p>12 jan, 2021 - 25 jan 2021</p>
+          <p>
+            Mount Fuji is the tallest mountain in Japan, standing at 3,776
+            meters (12,380 feet). Mount Fuji is the single most popular tourist
+            site in Japan, for both Japanese and foreign tourists.
+          </p>
+        </div>
+      </aside>
     </div>
   );
 }
