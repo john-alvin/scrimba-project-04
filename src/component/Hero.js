4f6d6bf6a56c5a1ad5b6b70704f8816a40457971
John Alvin <johnalvin.bonifacio@proton.me>
2022-11-18 08:24:40 +0800

    use props method for hero component
    add props to access information
    from data js file

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-function Hero() {
+function Hero(props) {
   return (
     <div className="container">
-      <img src="../images/places" alt="beautiful-place" />
+      <img src={`../images/${props.imageUrl}`} alt="beautiful-place" />
 
       <aside>
         <div className="top-head">
           <img src="../images/location-logo.png" alt="location-logo" />
-          <p>Japan</p>
-          <p>view on google maps</p>
+          <p className="location">{props.location}</p>
+          <a href={props.googleMapsUrl} target="_blank">
+            check it on google map
+          </a>
         </div>
         <div className="main-places">
-          <h1>Mount Fuji</h1>
-          <p>12 jan, 2021 - 25 jan 2021</p>
+          <h1>{props.title}</h1>
           <p>
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
+            {props.startDate} - {props.endDate}
           </p>
+          <p>{props.description}</p>
         </div>
       </aside>
     </div>
