62cc99f13e4760257222773d83923b1da0ba8a3b
John Alvin <johnalvin.bonifacio@proton.me>
2022-11-18 08:22:26 +0800

    use map method for data js file
    use map method for data js file for
    automatic looping of data when new
    information is being added
    import data js file
    use spread syntax for clean code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Data from "./Data";
+import Data from "./data";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
-
+import data from "./data";
 import "./App.css";
 
 function App() {
+  const heroMain = data.map((item) => {
+    return <Hero key={item.id} {...item} />;
+  });
   return (
     <div>
       <Navbar />
-      <Hero />
+      {heroMain}
     </div>
   );
 }
